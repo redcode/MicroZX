7360e976eb82d356e859313a5ad0d83e421f062e
redcode <manuel@redcodesoftware.com>
2015-06-24 19:50:39 +0200

    Remove folders for git sync (because OS X lacks case sensitiveness in filesystem paths)

--- Sources/Common/Machine.c
+++
@@ -1,7 +0,0 @@
-/*	_________  ___
-  _____ \_   /\  \/  /	μZX - common/MainController.h
- |  |  |_/  /__>    <	Copyright © 2014-2015 Manuel Sainz de Baranda y Goñi.
- |   ____________/\__\	Released under the terms of the GNU General Public License v3.
- |_*/
-
-#include "Machine.h"
